16697865599ea38a8647d4d4a90e4a2d00074713
stephanie hanne <flamme1994@hotmail.fr>
2025-07-18 13:35:21 +0200

    insta

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <section class="the one">
+        <h1>MINI INSTA</h1>
+        <p>Ajouté une photo !</p>
+        <div>
+            <form action="/index2.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="author" placeholder="Auteur"><br/>
+                <input type="file" name="fichier" placeholder="Parcourir ...">
+                <!-- <label for="name">Fichier</label> -->
+                <button>envoyer</button>
+            </form>
+        </div>
+        <img src="/photos/chateau 2.jpg" alt="chateau">
+    </section>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,37 @@
+<pre>
+    <?php 
+        // var_dump($_FILES);
+        $chemin = $_FILES["fichier"]["tmp_name"];
+        $fichier = $_FILES["fichier"]["name"];
+        move_uploaded_file($chemin, $fichier);
+?>
+</pre>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <section class="the one">
+            <h1>MINI INSTA</h1>
+            <p>Upload réussi !</p>
+        <div>
+            <!-- <img src="photos/chateau.jpg" alt="chateau"> -->
+            <?php 
+            $photos_dir = opendir("photos");
+                // do {
+                    $file_name = readdir($photos_dir);
+                    echo "<img src='photos/chateau.jpg'>";
+                // } 
+                // while ($file_name);
+            ?>
+        </div>
+            <a href="index.php">retour d'accueille</a>
+    </section>
+</body>
+</html>
